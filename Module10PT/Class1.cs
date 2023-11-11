2c290b20c769fa9b0de6e89a969db1af8e6038a7
Ser1kkhan <serikhana78@gmail.com>
2023-11-11 13:10:42 +0600

    Добавьте файлы проекта.

---
+++ Module10PT/Class1.cs
@@ -0,0 +1,175 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace HouseConstruction
+//{
+//    // Интерфейсы
+//    public interface IPart
+//    {
+//        bool IsBuilt { get; }
+//        void Build();
+//    }
+
+//    public interface IWorker
+//    {
+//        void Work(House house);
+//    }
+
+//    // Классы для частей дома
+//    public class House
+//    {
+
+//        public Basement Basement { get; set; }
+//        public List<Wall> Walls { get; private set; }
+//        public List<Window> Windows { get; private set; }
+//        public Door Door { get; set; }
+//        public Roof Roof { get; set; }
+
+
+//        public House()
+//        {
+//            Walls = new List<Wall>();
+//            Windows = new List<Window>();
+//        }
+
+//        public void ShowHouse()
+//        {
+//            Console.WriteLine("    ______ ");
+//            Console.WriteLine("   /      \\ ");
+//            Console.WriteLine("  /________\\ ");
+//            Console.WriteLine("  |   __   | ");
+//            Console.WriteLine("  |  |  |  | ");
+//            Console.WriteLine("  |  |__|  | ");
+//            Console.WriteLine("  |________| ");
+//        }
+//    }
+
+//    public class Basement : IPart
+//    {
+//        public bool IsBuilt { get; private set; }
+//        public void Build() => IsBuilt = true;
+//    }
+
+//    public class Wall : IPart
+//    {
+//        public bool IsBuilt { get; private set; }
+//        public void Build() => IsBuilt = true;
+//    }
+
+//    public class Window : IPart
+//    {
+//        public bool IsBuilt { get; private set; }
+//        public void Build() => IsBuilt = true;
+//    }
+
+//    public class Door : IPart
+//    {
+//        public bool IsBuilt { get; private set; }
+//        public void Build() => IsBuilt = true;
+//    }
+
+//    public class Roof : IPart
+//    {
+//        public bool IsBuilt { get; private set; }
+//        public void Build() => IsBuilt = true;
+//    }
+
+//    // Классы для работников
+//    public class Worker : IWorker
+//    {
+//        public void Work(House house)
+//        {
+//            if (house.Basement == null)
+//            {
+//                house.Basement = new Basement();
+//                house.Basement.Build();
+//            }
+//            else if (house.Walls.Count < 4)
+//            {
+//                var wall = new Wall();
+//                wall.Build();
+//                house.Walls.Add(wall);
+//            }
+//            else if (house.Windows.Count < 4)
+//            {
+//                var window = new Window();
+//                window.Build();
+//                house.Windows.Add(window);
+//            }
+//            else if (house.Door == null)
+//            {
+//                house.Door = new Door();
+//                house.Door.Build();
+//            }
+//            else if (house.Roof == null)
+//            {
+//                house.Roof = new Roof();
+//                house.Roof.Build();
+//            }
+//        }
+//    }
+
+//    public class TeamLeader : IWorker
+//    {
+//        public void Work(House house)
+//        {
+//            Console.WriteLine("Отчет бригадира:");
+//            Console.WriteLine($"Фундамент - {(house.Basement?.IsBuilt == true ? "построен" : "не построен")}");
+//            Console.WriteLine($"Стены - {house.Walls.Count(w => w.IsBuilt)}/4 построены");
+//            Console.WriteLine($"Окна - {house.Windows.Count(w => w.IsBuilt)}/4 установлены");
+//            Console.WriteLine($"Дверь - {(house.Door?.IsBuilt == true ? "установлена" : "не установлена")}");
+//            Console.WriteLine($"Крыша - {(house.Roof?.IsBuilt == true ? "установлена" : "не установлена")}");
+//        }
+//    }
+
+//    public class Team
+//    {
+//        private List<IWorker> workers;
+//        private TeamLeader leader;
+
+//        public Team(TeamLeader teamLeader)
+//        {
+//            workers = new List<IWorker>();
+//            leader = teamLeader;
+//        }
+
+//        public void AddWorker(Worker worker)
+//        {
+//            workers.Add(worker);
+//        }
+
+//        public void BuildHouse(House house)
+//        {
+//            while (house.Walls.Count < 4 || house.Windows.Count < 4 || house.Door == null || house.Roof == null)
+//            {
+//                foreach (var worker in workers)
+//                {
+//                    worker.Work(house);
+//                }
+//                leader.Work(house);
+//            }
+
+//            Console.WriteLine("Строительство дома завершено!");
+//            house.ShowHouse();
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            House house = new House();
+//            Team team = new Team(new TeamLeader());
+
+//            for (int i = 0; i < 4; i++)
+//            {
+//                team.AddWorker(new Worker());
+//            }
+
+//            team.BuildHouse(house);
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ Module10PT/DopolnitelnoeZadanie.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Linq;
+
+//public interface IStudent
+//{
+//    string Name { get; set; }
+//    string FullName { get; set; }
+//    int[] Grades { get; set; }
+
+//    string GetName();
+//    string GetFullName();
+//    double GetAvgGrade();
+//}
+
+//public class Student : IStudent
+//{
+//    public string Name { get; set; }
+//    public string FullName { get; set; }
+//    public int[] Grades { get; set; }
+
+//    public Student(string name, string fullName, int[] grades)
+//    {
+//        Name = name;
+//        FullName = fullName;
+//        Grades = grades;
+//    }
+
+//    public string GetName()
+//    {
+//        return Name;
+//    }
+
+//    public string GetFullName()
+//    {
+//        return FullName;
+//    }
+
+//    public double GetAvgGrade()
+//    {
+//        if (Grades == null || Grades.Length == 0)
+//        {
+//            throw new InvalidOperationException("No grades are available to calculate the average.");
+//        }
+//        return Grades.Average();
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        var student = new Student("Alex", "Alex Merser", new int[] { 90, 85, 93, 88 });
+//        Console.WriteLine($"Name: {student.GetName()}");
+//        Console.WriteLine($"Full Name: {student.GetFullName()}");
+//        Console.WriteLine($"Average Grade: {student.GetAvgGrade()}");
+//    }
+//}
